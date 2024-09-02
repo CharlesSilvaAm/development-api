fd28b959071b7698cd1b014d768122985e721f94
charles.silva <charles.silva@grupoicts.com.br>
2024-09-02 13:21:59 -0400

    Desenvolvimento API - Iniciando a Arquitetura e a primeira requisição POST

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
 import express from 'express'
+import publicRoutes from './routes/public.js'
+
+const app = express()
+app.use(express.json())
+
+app.use('/', publicRoutes)
+
+
+app.listen(3001, () => console.log("Servidor Rodando 🚀"))
